92d558a6164f2a76bceb1609d36cf88658d23e8d
Jeison Ferrer <jeison120120@hotmail.com>
2025-09-18 20:51:04 -0500

    Imagenes y md de ponentes ya linkeado y funcionando

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,12 +2,12 @@ import { defineCollection, z } from "astro:content";
 import { glob } from 'astro/loaders'; 
 
 const ponentes = defineCollection({
-    loader:glob({base: './ponentes', pattern: '**/*.md'}),
-    schema: z.object({
+    loader:glob({base: './src/content/ponentes', pattern: '**/*.md'}),
+    schema: ({image})=>z.object({
         id: z.string(),
         nombre: z.string(),
         description: z.string(),
-        foto: z.string(),
+        foto: image(),
         organizacion: z.string().optional(),
 
     }),
