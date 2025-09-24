8ec894e0d04b7bec2a02a36ee810f51c4135ff96
GuillermoGU24 <jguillermogu24@gmail.com>
2025-09-24 00:00:29 -0500

    fix: firebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    imageService: true,
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   vite: {
     plugins: [tailwindcss()],
-    ssr: {
-      noExternal: ["firebase-admin"],
-    },
   },
 });
